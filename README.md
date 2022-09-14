ce9305101d01c8e8fd9075976169e8cf961e7149
Gilas A <chunkz.ring@gmail.com>
2022-09-14 20:09:09 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,74 @@
+
+# Parse NIK
+
+Javascript Library yang memudahkan untuk melakukan parse dan validasi data beserta tanggal atas sebuah Nomor Induk Kependudukan (NIK)
+
+## ⭐ Fitur
+
+Parse NIK memiliki beberapa fungsi yang dapat digunakan sbb :
+  
+| Fungsi | Deskripsi  |
+|--|--|
+| isValid() | mengembalikan hasil dalam bentuk boolean atas validasi NIK |
+| parse() | mengembalikan hasil dalam bentuk object atas validasi NIK dengan tambahan data seperti jenis kelamin, tanggal lahir, provinsi, kabupaten / kota, kecamatan dan kodepos |
+
+Parse NIK juga memiliki beberapa pesan error sbb :
+
+| Pesan Error |
+|--|
+| NIK harus dalam bentuk angka |
+| Jumlah karakter NIK tidak sesuai |
+| Tanggal Lahir tidak sesuai |
+| Wilayah tidak ditemukan |
+
+## 🔧 Cara Install
+
+HTML Javascript
+
+```
+<script type="text/javascript" src="index.js"></script>
+<script>
+    const nik = 3329091003780012;
+    const hasil = parse(nik);
+    console.log(hasil);
+</script>
+```
+
+NodeJS
+
+Jalankan perintah pada terminal :
+
+```
+npm i parsenik
+```
+  
+Tambahkan kode baris berikut lalu jalankan aplikasi NodeJS :
+  
+```
+const parsenik = require("parse");
+const nik = 3329091003780012;
+const hasil = parsenik.parse(nik);
+console.log(hasil);
+```
+  
+Contoh Hasil :
+
+```
+{
+    "nik": 3329091003780012,
+    "valid": true,
+    "jenis_kelamin": "LAKI-LAKI",
+    "tanggal_lahir": "1978-03-10",
+    "provinsi": "JAWA TENGAH",
+    "kabupaten_kota": "KAB. BREBES",
+    "kecamatan": "BREBES",
+    "kodepos": "52216"
+}
+```
+
+## 🥔 Link Referensi
+  
+Library berikut dibuat dengan bantuan beberapa referensi sbb :
+
+[https://disdukcapil.pontianakkota.go.id/page/nomor-induk-kependudukan](https://disdukcapil.pontianakkota.go.id/page/nomor-induk-kependudukan)
+[https://sig.bps.go.id/bridging-kode/index](https://sig.bps.go.id/bridging-kode/index)
---
+++ index.js
@@ -0,0 +1,96 @@
+// const dayjs = require("dayjs");
+const moment = require("moment");
+const source = require("./source.json");
+
+/**
+ * Olah nik untuk mengetahui apakah valid atau tidak
+ *
+ * @param {number} nik
+ * @return {boolean}
+ * @private
+ */
+
+function isValid (nik) {
+  const result = parse(nik);
+  return result.valid;
+}
+
+/**
+ * Olah nik sehingga menghasilkan nilai valid, jenis kelamin, tanggal lahir, provinsi, kabupaten_kota, kecamatan, kodepos, kode unik
+ *
+ * @param {number} nik
+ * @return {object}
+ * @private
+ */
+
+function parse (nik) {
+  let resp = {
+    nik: nik,
+    valid: true,
+  };
+
+  try {
+    let data = {
+      jenis_kelamin: "LAKI-LAKI",
+      tanggal_lahir: "",
+      provinsi: "",
+      kabupaten_kota: "",
+      kecamatan: "",
+      kodepos: "",
+    };
+  
+    const tmpNIK = nik.toString();
+    if(typeof nik != "number") {
+      throw new Error("NIK harus dalam bentuk angka");
+    }
+    if(tmpNIK.length != 16) {
+      throw new Error("Jumlah karakter NIK tidak sesuai");
+    }
+
+    let tglLahir = tmpNIK.substring(6, 8);
+    let blnLahir = tmpNIK.substring(8, 10);
+    let thnLahir = tmpNIK.substring(10, 12);
+
+    // proses pencarian jenis kelamin
+    tglLahir > 40 && (data.jenis_kelamin = "PEREMPUAN");
+
+    // proses pencarian tanggal lahir
+    const curThn = new Date().getFullYear().toString().substring(-2);
+    if(data.jenis_kelamin == "PEREMPUAN") {
+      tglLahir -= 40;
+      tglLahir = tglLahir.toString().length > 1 ? tglLahir : `0${tglLahir}`;
+    }
+    thnLahir = ( thnLahir < curThn ? `20${thnLahir}` :  `19${thnLahir}`);
+    const tglLahirFull = `${thnLahir}-${blnLahir}-${tglLahir}`;
+    if(!moment(tglLahirFull).isValid()) {
+      throw new Error("Tanggal Lahir tidak sesuai");
+    }
+    data.tanggal_lahir = tglLahirFull;
+
+    // proses pencarian wilayah
+    const foundDistrict = source.some(prov =>
+      prov.kabupaten_kota.some(kabkot =>
+        kabkot.kecamatan.some(kec => {
+            const check = kec.kode === tmpNIK.substring(0, 6);
+            if(check) {
+              data.provinsi = prov.nama;
+              data.kabupaten_kota = kabkot.nama;
+              data.kecamatan = kec.nama;
+              data.kodepos = kec.kodepos;
+            }
+            return check;
+          })));
+
+    if(!foundDistrict) {
+      throw new Error("Wilayah tidak ditemukan");
+    };
+    resp = {...resp, ...data};
+  } catch (error) {
+    resp.valid = false;
+    resp.message = error.message;
+  }
+  
+  return resp;
+}
+
+console.log(parse(3329091003780012));
